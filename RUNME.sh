f225a920bed2084e5993ce61af0c9419e6e79d63
guolh2013@hotmail.com <guolh2013@hotmail.com>
2020-02-29 18:42:47 +0800

    open online pdf url directly

--- RUNME.sh
+++ RUNME.sh
@@ -1,17 +1,20 @@
 #!/bin/bash 
 
 serverPort="13531"
-imagedir="project/resources/imgdoc/"
-bookdir="project/resources/doc/"
-audiodir="project/resources/audio/"
-videodir="project/resources/video/"
+
+rsdir="project/resources"
+
+imagedir="$rsdir/imgdoc/"
+bookdir="$rsdir/doc/"
+audiodir="$rsdir/audio/"
+videodir="$rsdir/video/"
 
 echo "Have you placed all files into $(realpath $bookdir) properly?"
 cd project/tools;
-python3 lspdf.py $bookdir || echo " ! You must have  python3 in your  \$PATH and permit it to run in this program...  "
-python3 lsimg.py $imagedir || echo " ! You are suggested to place some images into $(realpath $imagedir) as the pages of a default document ...  "
-python3 lsaudio.py $audiodir || echo " ! You are suggested to place some audio files into $(realpath $audiodir)  ...  "
-python3 lsvideo.py $videodir || echo " ! You are suggested to place some audio files into $(realpath $videodir)  ...  "
+python3 lspdf.py $bookdir 2>/dev/null || echo " DOC_ERROR! You must have  python3 in your  \$PATH and permit it to run in this program...  "
+python3 lsimg.py $imagedir 2>/dev/null  || echo " IMG_ERROR! You are suggested to place some images into $(realpath $imagedir) as the pages of a default document ...  "
+python3 lsaudio.py $audiodir 2>/dev/null || echo " AUDIO_ERROR! You are suggested to place some audio files into $(realpath $audiodir)  ...  "
+python3 lsvideo.py $videodir 2>/dev/null || echo " VIDEO_NOTFOUND! You are suggested to place some audio files into $(realpath $videodir)  ...  "
 cd ..
 
 echo ;
